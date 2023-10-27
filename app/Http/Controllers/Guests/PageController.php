c8cd1900ab21905acbd22194dcf825ffbb53eb39
RiccardoSansone <r.sansone@gmail.com>
2023-10-27 19:43:24 +0200

    add  card

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    public function index(){
+        return view('welcome', ['movies'=>Movie::all()]);
+    }
+}
